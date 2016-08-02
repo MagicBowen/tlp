205614a5acea3382c4b4de0f437c807f908eab70
MagicBowen <e.bowen.wang@icloud.com>
2016-08-03 06:31:49 +0800

    modify the fixture name defination

--- include/tlp/test/Test.h
+++ include/tlp/test/Test.h
@@ -26,9 +26,9 @@ static_assert(__value(__is_eq(T, Expected)), "TLP Error: expect "#T" be equal to
 #define ASSERT_NE(T, Expected)          \
 static_assert(!(__value(__is_eq(T, Expected))), "TLP Error: expect "#T" be not equal to "#Expected"!")
 
-#define FIXTURE(Name) namespace Name
+#define FIXTURE(Name) namespace _JOIN(TlpFixture_, Name)
 
-#define TEST(Name) struct UNIQUE_NAME(Test_)
+#define TEST(Name) struct UNIQUE_NAME(TlpTest_)
 
 #define TLP_RUN_ALL_TESTS()             \
 int main() { std::cout << "TLP RUN ALL TESTS OK!!!" << std::endl; return 0; }
